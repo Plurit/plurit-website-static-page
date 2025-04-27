a1534a912bd55106dc85a5656611801b61d360a8
Deepak Kumar <dpk950@gmail.com>
2025-04-27 15:30:15 +0530

    feat: add vite with react
    By also supporting the current web pages

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="home-iframe-div">
+      <iframe src="/home.html" title="Plurit" className="home-iframe" />
+    </div>
+  );
+}
+
+export default App;
